0a21dca8914c988064051324c19f0696fc6fc28e
Mashiur Rahman <rmashiur15@gmail.com>
2018-03-21 18:16:26 +0600

    Complete be a donor design

--- app/src/main/java/com/example/user/bloodapp/BeAdonor.java
+++ app/src/main/java/com/example/user/bloodapp/BeAdonor.java
@@ -1,13 +1,47 @@
 package com.example.user.bloodapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BeAdonor extends AppCompatActivity {
+
+    String[] location_names;
+    Button locationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_be_adonor);
+
+        location_names = getResources().getStringArray(R.array.location_names);
+        locationBtn = findViewById(R.id.locationSelectBtn);
+    }
+
+    public void LocationBtn(View view) {
+
+        AlertDialog.Builder builder =new AlertDialog.Builder(BeAdonor.this);
+        builder.setTitle("Choose Your Location");
+        builder.setItems(location_names, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                locationBtn.setText(location_names[i]);
+            }
+        });
+
+        builder.setCancelable(false);
+        builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
--- app/src/main/java/com/example/user/bloodapp/SplashScreen.java
+++ app/src/main/java/com/example/user/bloodapp/SplashScreen.java
@@ -1,12 +1,15 @@
 package com.example.user.bloodapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+    TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,11 @@ public class SplashScreen extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        welcomeText = findViewById(R.id.welcomeText);
+
+        Typeface myCustomFonts = Typeface.createFromAsset(getAssets(),"dayslater.ttf");
+        welcomeText.setTypeface(myCustomFonts);
 
         final Thread thread = new Thread(){
             @Override
